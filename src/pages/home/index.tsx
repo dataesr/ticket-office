b2459766ad2cef9351aa7e69d745ea388c541080
Mihoub Debache <debache.mihoub@gmail.com>
2024-04-24 18:40:16 +0200

    test

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import { Container } from "@dataesr/dsfr-plus";
 const Home = () => {
   return (
     <Container className="fr-mt-10v">
+      {console.log(VITE_SCANR_API_AUTHORIZATION)}
       <h1>Bienvenue sur scanR Contact v2 !</h1>
       <p>Contenu de la page d'accueil...</p>
     </Container>
