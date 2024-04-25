450d40060a86ed520f44c4201bacdd96dd81706a
Mihoub Debache <debache.mihoub@gmail.com>
2024-04-25 17:01:20 +0200

    fix(ci): set custom header

--- src/api/contribution-api/useGetObjectContributeData.tsx
+++ src/api/contribution-api/useGetObjectContributeData.tsx
@@ -5,6 +5,7 @@ function useGetContributionData(URL: unknown, reload: unknown) {
   const [data, setData] = useState({});
   const [isLoading, setLoading] = useState(true);
   const [isError, setError] = useState(false);
+  console.log(postHeaders);
   useEffect(() => {
     async function getData() {
       const url = URL as RequestInfo;
