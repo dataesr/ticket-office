19ec23da54e828db444ffd82fbaa2e86d4fb93f4
Mihoub Debache <debache.mihoub@gmail.com>
2024-04-25 17:07:53 +0200

    fix(ci): update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { config } from "dotenv";
+
+config({ path: "./.env.local" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
