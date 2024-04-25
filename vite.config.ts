947a305eff5dc77ee2696ceb589a9953e33f9a7b
Mihoub <debache.mihoub@gmail.com>
2024-04-25 17:13:31 +0200

    Revert "fix(ci): update vite config"
    This reverts commit 14c846c0e1f1fb0061cd5732425b240a756cefc6.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { config } from "dotenv";
-
-config({ path: "./.env.local" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
