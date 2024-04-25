e0fc1c9e46423cd842eeada49a34a692ee94491b
Mihoub <debache.mihoub@gmail.com>
2024-04-25 18:33:20 +0200

    fix(ci): update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const env = loadEnv(process.env.MODE, process.cwd());
+
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  preview: {
-    port: 5173,
-    strictPort: true,
-  },
   server: {
     port: 5173,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:5173",
+    cors: true,
+    headers: {
+      "Access-Control-Allow-Origin": "http://0.0.0.0:5173",
+    },
+  },
+  define: {
+    VITE_SCANR_API_AUTHORIZATION: JSON.stringify(
+      env.VITE_SCANR_API_AUTHORIZATION
+    ),
   },
 });
