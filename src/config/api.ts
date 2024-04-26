00c26d6eb4842d05d604fbdd6452b4364e43462a
Mihoub <debache.mihoub@gmail.com>
2024-04-26 14:53:47 +0200

    fix(ci): add env config for typescript

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
-const { VITE_SCANR_API_AUTHORIZATION: API_KEY } = import.meta.env;
+const API_KEY = import.meta.env.VITE_SCANR_API_AUTHORIZATION;
 export const headers = API_KEY ? { Authorization: `Basic ${API_KEY}` } : {};
 export const postHeaders = { ...headers, "Content-Type": "application/json" };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+interface ImportMeta {
+  env: {
+    [key: string]: string;
+    VITE_SCANR_API_AUTHORIZATION?: string;
+  };
+}
