6b843af2ef10b2fe48ac375ee370f3f0e5e56e1b
Mihoub <debache.mihoub@gmail.com>
2024-05-02 11:53:32 +0200

    fix(improvements): add searchParams to searchBar and made compoents to clean code

---
+++ src/components/pagination/top-buttons.tsx
@@ -0,0 +1,33 @@
+import { Button, Text } from "@dataesr/dsfr-plus";
+
+const TopPaginationButtons = ({ meta, page, maxPage, setPage }) => {
+  return (
+    <>
+      <Text size="sm" bold>
+        Résultats: 1-20 de {meta.total}
+      </Text>
+      <Button
+        onClick={() => setPage(1)}
+        disabled={page === 1}
+        title="Page 1"
+        className="fr-mr-1w"
+        variant="secondary"
+        size="sm"
+      >
+        Retour à la page 1
+      </Button>
+      <Button
+        onClick={() => setPage(maxPage)}
+        disabled={page === maxPage}
+        title={`Page ${maxPage}`}
+        className="fr-mr-1w"
+        variant="secondary"
+        size="sm"
+      >
+        Dernière page
+      </Button>
+    </>
+  );
+};
+
+export default TopPaginationButtons;
