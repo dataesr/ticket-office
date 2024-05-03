223528cb60df9b796360846d75325577c4875469
Mihoub <debache.mihoub@gmail.com>
2024-05-03 14:43:10 +0200

    fix(layout): add svg and clean header and footer

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,13 +3,8 @@ import { Container } from "@dataesr/dsfr-plus";
 const Home = () => {
   return (
     <Container className="fr-mt-10v">
-      <h1>Bienvenue sur scanR Contact v2 !</h1>
-      <h1>
-        Ceci est un test pour voir si quand je build ça prend bien la dernière
-        image
-      </h1>
-
-      <p>Contenu de la page d'accueil...</p>
+      <h1>Bienvenue sur le Guichet numérique du DIST</h1>
+      <h1>Bientôt des gaphiques</h1>
     </Container>
   );
 };
