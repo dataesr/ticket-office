4ba9f99482402fb8381e82ad282f2e844754dc37
Mihoub <debache.mihoub@gmail.com>
2024-05-13 13:53:59 +0200

    fix(modal): put a tag or change a status now modify scanr api

--- src/api/send-mail/index.tsx
+++ src/api/send-mail/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { Button, Text, TextArea } from "@dataesr/dsfr-plus";
+import { Button, Col, Row, Text, TextArea } from "@dataesr/dsfr-plus";
 import SibApiV3Sdk from "sib-api-v3-sdk";
 import { Contribution } from "../../types";
+import { postHeaders } from "../../config/api";
 
 function EmailSender({ contribution }: { contribution: Contribution }) {
   const [emailSent, setEmailSent] = useState(false);
   const [response, setResponse] = useState("");
-
   const { VITE_BREVO_API_AUTHORIZATION } = import.meta.env;
 
   const sendEmail = async () => {
@@ -28,29 +28,53 @@ function EmailSender({ contribution }: { contribution: Contribution }) {
           email: "debache.mihoub@gmail.com",
         },
       ];
-      sendSmtpEmail.subject = "My subject";
+      sendSmtpEmail.subject = "Réponse à votre contribution";
       sendSmtpEmail.htmlContent = response;
 
       await apiInstance.sendTransacEmail(sendSmtpEmail);
+
+      const data = {
+        comment: response,
+      };
+
+      const responsePatch = await fetch(
+        `https://scanr-api.staging.dataesr.ovh/contributions/${contribution.id}`,
+        {
+          method: "PATCH",
+          headers: postHeaders,
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (!responsePatch.ok) {
+        console.log("Erreur lors de la mise à jour de la contribution");
+        return;
+      }
+
       setEmailSent(true);
     } catch (error) {
-      console.error("Error sending email:", error);
+      console.error(
+        "Erreur lors de l'envoi de l'email ou de la mise à jour de la contribution:",
+        error
+      );
     }
   };
 
   return (
-    <div>
-      <TextArea
-        value={response}
-        onChange={(e) => setResponse(e.target.value)}
-        placeholder="Votre réponse..."
-        rows={2}
-      />
-      <Button variant="secondary" onClick={sendEmail} className="fr-mb-1w">
+    <Row gutters>
+      <Col offsetMd="2">
+        <TextArea
+          value={response}
+          onChange={(e) => setResponse(e.target.value)}
+          placeholder="Votre réponse..."
+          rows={2}
+        />
+      </Col>
+      <Button variant="secondary" onClick={sendEmail} size="sm">
         {contribution.comment ? "Renvoyer un mail" : "Répondre"}
       </Button>
       {emailSent && <Text>Mail envoyé!</Text>}
-    </div>
+    </Row>
   );
 }
 
