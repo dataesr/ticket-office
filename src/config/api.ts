1afa1219c79b37b79c8e24f920228486170f1700
Mihoub <debache.mihoub@gmail.com>
2024-05-17 11:30:06 +0200

    fix(graphs): update graph style

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,5 +2,7 @@ const API_KEY = import.meta.env.VITE_SCANR_API_AUTHORIZATION;
 export const headers = API_KEY ? { Authorization: `Basic ${API_KEY}` } : {};
 export const postHeaders = { ...headers, "Content-Type": "application/json" };
 
-export const contributionUrl = "/api/contribute?max_results=2000";
-export const contactUrl = "/api/contact?max_results=2000";
+export const contributionUrl =
+  "https://scanr-api.dataesr.ovh/contribute?max_results=2000";
+export const contactUrl =
+  "https://scanr-api.dataesr.ovh/contact?max_results=2000";
