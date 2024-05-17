27178a852dba229a3f56fbc6dd6f74de4b0f370e
Mihoub <debache.mihoub@gmail.com>
2024-05-17 10:51:42 +0200

    fix(graphs): add api to url for graphs

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,6 +3,6 @@ export const headers = API_KEY ? { Authorization: `Basic ${API_KEY}` } : {};
 export const postHeaders = { ...headers, "Content-Type": "application/json" };
 
 export const contributionUrl =
-  "https://scanr-api.dataesr.ovh/contribute?max_results=2000";
+  "https://scanr-api.dataesr.ovh/api/contribute?max_results=2000";
 export const contactUrl =
-  "https://scanr-api.dataesr.ovh/contact?max_results=2000";
+  "https://scanr-api.dataesr.ovh/api/contact?max_results=2000";
