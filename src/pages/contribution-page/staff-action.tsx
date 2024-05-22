6a6ce602e59c307d0b0984d245cca74ffc01380a
Mihoub <debache.mihoub@gmail.com>
2024-05-22 16:59:57 +0200

    fix(email): instant update state to display new sent mail

--- src/pages/contribution-page/staff-action.tsx
+++ src/pages/contribution-page/staff-action.tsx
@@ -2,25 +2,26 @@ import { Col, Text } from "@dataesr/dsfr-plus";
 import EmailSender from "../../api/send-mail";
 import type { Contribution } from "../../types";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const StaffActions = ({ data }: { data: Contribution }) => {
   const location = useLocation();
+  const [responseScanR, setResponseScanR] = useState(null);
   const contributorClassName = location.pathname.includes("contributionpage")
     ? "staffSide"
     : "staffSideContact";
-
   return (
     <>
       {data?.comment && (
         <Col className={contributorClassName}>
           <Text size="sm">
-            Réponse apportée par {data?.responseFrom || data.team[0]} le{" "}
-            {new Date(data?.modified_at).toLocaleDateString()}{" "}
+            Réponse apportée par {responseScanR?.responseFrom || data.team[0]}{" "}
+            le {new Date(data?.modified_at).toLocaleDateString()}{" "}
           </Text>
-          <Text>{data?.comment}</Text>
+          <Text>{responseScanR?.comment || data.comment}</Text>
         </Col>
       )}
-      <EmailSender contribution={data} />
+      <EmailSender contribution={data} setResponseScanR={setResponseScanR} />
     </>
   );
 };
