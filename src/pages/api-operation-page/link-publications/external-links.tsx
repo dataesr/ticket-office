2fce4bf55b6edce52117edbcf56ba1bff495a689
Mihoub <debache.mihoub@gmail.com>
2024-06-20 08:58:07 +0200

    fix(links): change link color when clicked

--- src/pages/api-operation-page/link-publications/external-links.tsx
+++ src/pages/api-operation-page/link-publications/external-links.tsx
@@ -1,22 +1,31 @@
 import { Link } from "@dataesr/dsfr-plus";
+import { useState } from "react";
 
 export const ExternalLinks = ({ productionId, name }) => {
   const formattedProductionId = productionId.replace(/\//g, "%2f");
+  const [google, setGoogleClicked] = useState(false);
+  const [scanRClicked, setScanRClicked] = useState(false);
   return (
     <>
       <Link
-        className="fr-ml-5w fr-mr-5w"
+        className={`fr-ml-5w fr-mr-5w fr-footer__content-link ${
+          scanRClicked ? "clicked-link" : ""
+        }`}
         target="_blank"
         rel="noreferrer noopener external"
         href={`https://scanr.enseignementsup-recherche.gouv.fr/publications/${formattedProductionId}`}
+        onClick={() => setScanRClicked(true)}
       >
         scanR
       </Link>
       <Link
-        className="fr-mr-1w"
+        className={`fr-mr-1w fr-footer__content-link ${
+          google ? "clicked-link" : ""
+        }`}
         target="_blank"
         rel="noreferrer noopener external"
         href={`https://google.com/search?q=${name}`}
+        onClick={() => setGoogleClicked(true)}
       >
         Google
       </Link>
