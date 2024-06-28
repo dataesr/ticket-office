d0ac07e5e9a188dc6ba9597366e9b8f4418a2eb5
Mihoub <debache.mihoub@gmail.com>
2024-06-28 14:34:17 +0200

    fix(email-sender): change mail adresse for sender

--- src/pages/api-operation-page/link-publications/data-list-context/index.tsx
+++ src/pages/api-operation-page/link-publications/data-list-context/index.tsx
@@ -1,22 +1,19 @@
 import React, { createContext, useState, useContext } from "react";
 
-// Définition du type de contexte
 interface DataListContextType {
-  dataList: any[]; // Type de dataList à ajuster selon votre structure
+  dataList: any[];
   setDataList: React.Dispatch<React.SetStateAction<any[]>>;
 }
 
-// Création du contexte avec une valeur par défaut
 const DataListContext = createContext<DataListContextType>({
   dataList: [],
   setDataList: () => {},
 });
 
-// Composant Provider pour le contexte
 export const DataListProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [dataList, setDataList] = useState<any[]>([]); // Initialisation de dataList avec un tableau vide
+  const [dataList, setDataList] = useState<any[]>([]);
 
   return (
     <DataListContext.Provider value={{ dataList, setDataList }}>
@@ -25,7 +22,6 @@ export const DataListProvider: React.FC<{ children: React.ReactNode }> = ({
   );
 };
 
-// Hook personnalisé pour utiliser le contexte
 export const useDataList = () => {
   return useContext(DataListContext);
 };
