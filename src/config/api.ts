85e4da84a63fa2c56efaa2b65c1fc204f6847214
Mihoub <debache.mihoub@gmail.com>
2024-09-10 15:15:11 +0200

    fix(url): re set build url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,9 @@
 const API_KEY = import.meta.env.VITE_SCANR_API_AUTHORIZATION;
 export const headers = API_KEY ? { Authorization: `Basic ${API_KEY}` } : {};
-export const postHeaders = { ...headers, "Content-Type": "application/json" };
+export const postHeaders = {
+  ...headers,
+  "Content-Type": "application/json",
+};
 
 const isDevelopment = import.meta.env.VITE_HEADER_TAG === "Development";
 export const contributionUrl = isDevelopment
