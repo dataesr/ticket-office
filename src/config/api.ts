ddcb3e814e9ba1a7d3d9a11e789306f8caefd6d9
Mihoub <debache.mihoub@gmail.com>
2024-09-10 14:44:27 +0200

    fix(url): fix url to request

--- src/config/api.ts
+++ src/config/api.ts
@@ -5,12 +5,12 @@ export const postHeaders = { ...headers, "Content-Type": "application/json" };
 const isDevelopment = import.meta.env.VITE_HEADER_TAG === "Development";
 export const contributionUrl = isDevelopment
   ? "http://localhost:3000/api/contribute?max_results=2000"
-  : "https://ticket-office-api.staging/api/contribute?max_results=2000";
+  : "https://ticket-office-api.staging.dataesr.ovh/api/contribute?max_results=2000";
 
 export const contactUrl = isDevelopment
   ? "http://localhost:3000/api/contact?max_results=2000"
-  : "https://ticket-office-api.staging/api/contact?max_results=2000";
+  : "https://ticket-office-api.staging.dataesr.ovh/api/contact?max_results=2000";
 
 export const productionUrl = isDevelopment
   ? "http://localhost:3000/api/production?max_results=2000"
-  : "https://ticket-office-api.staging/api/productionsmax_results=2000";
+  : "https://ticket-office-api.staging.dataesr.ovh/api/productionsmax_results=2000";
