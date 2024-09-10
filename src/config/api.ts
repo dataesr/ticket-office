218b93278143466bb589ad174dbaa745bbf29d0c
Mihoub <debache.mihoub@gmail.com>
2024-09-10 15:31:14 +0200

    typo

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,11 @@
 const API_KEY = import.meta.env.VITE_SCANR_API_AUTHORIZATION;
+const isDevelopment = import.meta.env.VITE_HEADER_TAG === "Development";
+console.log(`API_KEY: ${API_KEY}`);
 export const headers = API_KEY ? { Authorization: `Basic ${API_KEY}` } : {};
 export const postHeaders = {
   ...headers,
   "Content-Type": "application/json",
 };
-const isDevelopment = import.meta.env.VITE_HEADER_TAG === "Development";
 
 export const contributionUrl = isDevelopment
   ? "http://localhost:3000/api/contribute?max_results=2000"
