011c34061993bc95b78b3e25de1c445af541f1a3
Mihoub <debache.mihoub@gmail.com>
2024-09-17 10:46:27 +0200

    fix(ci): add outdir in viteconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(), visualizer() as PluginOption],
-=======
   plugins: [react(), (visualizer as any).default() as PluginOption],
 >>>>>>> 3fa33f3 (refactor(ci): mix ui and api in one repo)
   resolve: {
