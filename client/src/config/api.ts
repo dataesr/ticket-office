576dfb21e78e05224861aea7500be06013336c7b
Mihoub <debache.mihoub@gmail.com>
2024-09-18 11:01:09 +0200

    fix(api): change url for request on staging

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -8,20 +8,20 @@ export const postHeaders = {
 
 export const contributionUrl = isDevelopment
   ? "http://localhost:3000/api/contribute?max_results=2000"
-  : "https://ticket-office-api.staging.dataesr.ovh/api/contribute?max_results=2000";
+  : "https://ticket-office.staging.dataesr.ovh/api/contribute?max_results=2000";
 
 export const contactUrl = isDevelopment
   ? "http://localhost:3000/api/contact?max_results=2000"
-  : "https://ticket-office-api.staging.dataesr.ovh/api/contact?max_results=2000";
+  : "https://ticket-office.staging.dataesr.ovh/api/contact?max_results=2000";
 
 export const productionUrl = isDevelopment
   ? "http://localhost:3000/api/production?max_results=2000"
-  : "https://ticket-office-api.staging.dataesr.ovh/api/productionsmax_results=2000";
+  : "https://ticket-office.staging.dataesr.ovh/api/productionsmax_results=2000";
 
 export const nameChangeUrl = isDevelopment
   ? "http://localhost:3000/api/update-user-data?max_results=2000"
-  : "https://ticket-office-api.staging.dataesr.ovh/api/update-user-datamax_results=2000";
+  : "https://ticket-office.staging.dataesr.ovh/api/update-user-datamax_results=2000";
 
 export const removeUserUrl = isDevelopment
   ? "http://localhost:3000/api/remove-user?max_results=2000"
-  : "https://ticket-office-api.staging.dataesr.ovh/api/remove-usermax_results=2000";
+  : "https://ticket-office.staging.dataesr.ovh/api/remove-usermax_results=2000";
