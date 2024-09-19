0f22816baa8403d5dfda0d7c77052299706f266f
Mihoub <debache.mihoub@gmail.com>
2024-09-19 10:33:14 +0200

    fix(schema): add optional items

--- server/schemas/put/contactSchema.ts
+++ server/schemas/put/contactSchema.ts
@@ -5,6 +5,7 @@ export const putContactSchema = t.Object(
     idref: t.Optional(t.String()),
     status: t.Optional(t.String()),
     team: t.Optional(t.Array(t.String())),
+    tags: t.Optional(t.Array(t.String())),
     comment: t.Optional(t.String()),
   },
   { additionalProperties: false }
