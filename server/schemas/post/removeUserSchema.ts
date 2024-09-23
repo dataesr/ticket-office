04af7897b747f152cf35b3335e5cb8415ec46af1
Mihoub <debache.mihoub@gmail.com>
2024-09-23 10:18:32 +0200

    fix(router): update router, clean types, and requier things in post schema

--- server/schemas/post/removeUserSchema.ts
+++ server/schemas/post/removeUserSchema.ts
@@ -3,10 +3,10 @@ import { t } from "elysia";
 export const postRemoveUserSchema = t.Object(
   {
     email: t.Required(t.String()),
-    name: t.String(),
-    message: t.String(),
+    name: t.Required(t.String()),
+    message: t.Required(t.String()),
     organisation: t.Optional(t.String()),
-    collectionName: t.String(),
+    collectionName: t.Required(t.String()),
     fonction: t.Optional(t.String()),
     idref: t.Optional(t.String()),
     status: t.Optional(t.String()),
