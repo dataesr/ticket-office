7077f11a29614e82fad6c56f040132bc3b602426
Mihoub <debache.mihoub@gmail.com>
2024-09-24 15:16:52 +0200

    fix(schema): update post schema for contact

--- server/schemas/post/productionsSchema.ts
+++ server/schemas/post/productionsSchema.ts
@@ -2,16 +2,16 @@ import { t } from "elysia";
 
 export const postProductionsSchema = t.Object(
   {
-    email: t.Required(t.String()),
-    name: t.Required(t.String()),
-    message: t.Required(t.String()),
+    email: t.String(),
+    name: t.String(),
+    message: t.String(),
     created_at: t.Optional(t.Date()),
     organisation: t.Optional(t.String()),
-    collectionName: t.Required(t.String()),
+    collectionName: t.String(),
     fonction: t.Optional(t.String()),
     idref: t.Optional(t.String()),
     status: t.Optional(t.String()),
-    productions: t.Required(
+    productions: t.Optional(
       t.Array(
         t.Object({
           id: t.String(),
