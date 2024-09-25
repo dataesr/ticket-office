5f1750dfc863c789f8fa45725370c8baec4a0b6f
Mihoub <debache.mihoub@gmail.com>
2024-09-25 16:24:19 +0200

    fix(mongo): add directionConnection

--- server/libs/mongo.ts
+++ server/libs/mongo.ts
@@ -2,6 +2,7 @@ import { MongoClient } from "mongodb";
 
 const mongoUri = process.env.MONGO_URI || "";
 <<<<<<< HEAD
+<<<<<<< HEAD
 const mongoDatabase = process.env.MONGO_DATABASE || "";
 const { MONGO_URI = mongoUri, MONGO_DATABASE = mongoDatabase } = Bun.env;
 const client = new MongoClient(MONGO_URI);
@@ -9,6 +10,9 @@ const client = new MongoClient(MONGO_URI);
 const db = client.db(MONGO_DATABASE);
 =======
 const client = new MongoClient(mongoUri);
+=======
+const client = new MongoClient(mongoUri, { directConnection: true });
+>>>>>>> 3f323e3 (fix(mongo): add directionConnection)
 const db = client.db("ticket-office-api");
 >>>>>>> 3fa33f3 (refactor(ci): mix ui and api in one repo)
 
