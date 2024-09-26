23fbd28c6ef70e8a67b8e96a0913f03dfe0b9a1b
Mihoub <debache.mihoub@gmail.com>
2024-09-26 12:20:24 +0200

    typo

--- server/routes/contacts/patch/index.ts
+++ server/routes/contacts/patch/index.ts
@@ -64,7 +64,7 @@ contactPutRoutes.patch(
       summary: "Modifier une contribution via formulaire de contact par ID",
       description:
         "Cette route permet de mettre à jour une contribution spécifique via l'ID fourni. Elle permet de modifier le statut, l'idref, d'ajouter la personne modifiant dans l'équipe, et de mettre à jour la date de traitement et de modification.",
-      tags: ["Contact"],
+      tags: ["Contacts"],
     },
   }
 );
