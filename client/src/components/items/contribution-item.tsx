ff939e68d1249d082bb960b210a2995d1cf0ec2c
Mihoub <debache.mihoub@gmail.com>
2024-09-30 15:28:29 +0200

    fix(naming): clean code

--- client/src/components/items/contribution-item.tsx
+++ client/src/components/items/contribution-item.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Badge, Col, Row, Text, Notice, Title } from "@dataesr/dsfr-plus";
 import ContributorInfo from "./contributor-info";
-import StaffActions from "../../components/contact/staff-action";
+import StaffActions from "./staff-action";
 import { ContributionItemProps } from "../../types";
 import {
   BadgeColor,
