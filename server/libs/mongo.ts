0a9c709f0a8607fc6118d1b69c93946aa56527c5
Mihoub <debache.mihoub@gmail.com>
2024-10-07 09:50:41 +0200

    typo

--- server/libs/mongo.ts
+++ server/libs/mongo.ts
@@ -7,6 +7,7 @@ const mongoUri = process.env.MONGO_URI || "";
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const mongoDatabase = process.env.MONGO_DATABASE || "";
 const { MONGO_URI = mongoUri, MONGO_DATABASE = mongoDatabase } = Bun.env;
 const client = new MongoClient(MONGO_URI);
@@ -30,6 +31,10 @@ const db = client.db("ticket-office-api");
 >>>>>>> 3fa33f3 (refactor(ci): mix ui and api in one repo)
 =======
 const { MONGO_URI = mongoUri, MONGO_DATABASE = mongoUri } = Bun.env;
+=======
+const mongoDatabase = process.env.MONGO_DATABASE || "";
+const { MONGO_URI = mongoUri, MONGO_DATABASE = mongoDatabase } = Bun.env;
+>>>>>>> d9764c0 (typo)
 const client = new MongoClient(MONGO_URI);
 
 const db = client.db(MONGO_DATABASE);
