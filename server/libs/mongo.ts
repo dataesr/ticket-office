01df9a715286fc8d8f21dcb86ce8c8d8d9181bbc
Mihoub <debache.mihoub@gmail.com>
2024-10-07 09:49:45 +0200

    fix(mongo): update connexion to mongo

--- server/libs/mongo.ts
+++ server/libs/mongo.ts
@@ -1,7 +1,9 @@
 import { MongoClient } from "mongodb";
 
 const mongoUri = process.env.MONGO_URI || "";
-const client = new MongoClient(mongoUri);
-const db = client.db("ticket-office-api");
+const { MONGO_URI = mongoUri, MONGO_DATABASE = mongoUri } = Bun.env;
+const client = new MongoClient(MONGO_URI);
+
+const db = client.db(MONGO_DATABASE);
 
 export default db;
