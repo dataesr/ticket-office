40d7d5da744fd22449240ef2f6c960838b4f401e
Mihoub <debache.mihoub@gmail.com>
2024-10-07 09:50:41 +0200

    typo

--- server/libs/mongo.ts
+++ server/libs/mongo.ts
@@ -1,7 +1,8 @@
 import { MongoClient } from "mongodb";
 
 const mongoUri = process.env.MONGO_URI || "";
-const { MONGO_URI = mongoUri, MONGO_DATABASE = mongoUri } = Bun.env;
+const mongoDatabase = process.env.MONGO_DATABASE || "";
+const { MONGO_URI = mongoUri, MONGO_DATABASE = mongoDatabase } = Bun.env;
 const client = new MongoClient(MONGO_URI);
 
 const db = client.db(MONGO_DATABASE);
