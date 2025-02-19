460b3e7c2597bd3a58b792dac77999f02cb887bf
victor barbier <victor.barbier@recherche.gouv.fr>
2025-02-19 13:26:59 +0100

    fix(packages): correct packages location

--- client/src/pages/bso-local-variations/actions/upload-file.ts
+++ client/src/pages/bso-local-variations/actions/upload-file.ts
@@ -1,3 +1,5 @@
-export default function UploadFile() {
+import { Variation } from "../types"
+
+export default async function UploadFile(variation: Variation) {
   console.log("Update file to object storage... TBD")
 }
