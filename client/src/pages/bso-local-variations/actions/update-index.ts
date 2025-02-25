ced34cb67b12d5deb728e37f663d27322c132154
victor barbier <victor.barbier@recherche.gouv.fr>
2025-02-25 09:27:50 +0100

    feat(variations): prepare update index

--- client/src/pages/bso-local-variations/actions/update-index.ts
+++ client/src/pages/bso-local-variations/actions/update-index.ts
@@ -1,19 +1,42 @@
 export default function UpdateIndex() {
-  console.log("Update elasticsearch index... TBD")
-
-  // const data = {}
-
-  // fetch("/bso-publications", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(data),
+  // const upw = Bun.env.URL_UPW
+  // const password = Bun.env.PUBLIC_API_PASSWORD
+  // const chunksize = 5000
+  // const hal_dates = ["20221201", "20231214", "20241201"]
+  // const index = "bso-publications-20250102"
+  // const observations = ["20180927", "20191122", "20201009", "20211201", "20221201", "20231214", "20241203"]
+  // const skip_download = false
+  // const these_date = "20241201"
+  // const data: any = {
+  //   reset_file: false,
+  //   extract: false,
+  //   transform: true,
+  //   PUBLIC_API_PASSWORD: password,
+  //   index_name: index,
+  //   observations: observations,
+  //   hal_dates: hal_dates,
+  //   theses_date: these_date,
+  //   datasources: ["medline/24", "parsed_fr", "crossref_fr", "hal", "theses", "fixed", "local", "bso3"],
+  //   skip_download: skip_download,
+  //   chunksize: chunksize,
+  // }
+  // const chunks = [1, 2, 3, 4, 5, 6, 7, 8]
+  // chunks.forEach((idx) => {
+  //   data.split_idx = idx
+  //   fetch(`${upw}/et`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(data),
+  //   })
+  //     .then((response) => {
+  //       console.log(response)
+  //     })
+  //     .catch((error) => {
+  //       throw new Error(error)
+  //     })
   // })
-  //   .then((response) => {
-  //     console.log(response)
-  //   })
-  //   .catch((error) => {
-  //     throw new Error(error)
-  //   })
+
+  console.log("Updating index.... TBD")
 }
