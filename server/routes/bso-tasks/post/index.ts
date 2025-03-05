5bfe6b4e8489246e900125632d97c9c18cbc4b13
victor barbier <victor.barbier@recherche.gouv.fr>
2025-03-05 16:44:30 +0100

    feat(bso-tasks): add ids in respnse schema

--- server/routes/bso-tasks/post/index.ts
+++ server/routes/bso-tasks/post/index.ts
@@ -9,6 +9,7 @@ const updateIndex = new Elysia()
 const bodySchema = t.Record(t.String(), t.Union([t.Boolean(), t.String(), t.Number(), t.Array(t.String())]))
 const responseSchema = t.Object({
   chunks: t.Number(),
+  ids: t.Array(t.String()),
   params: bodySchema,
 })
 type bodyType = Static<typeof bodySchema>
@@ -17,6 +18,7 @@ updateIndex.post(
   "/bso-tasks",
   async ({ error, body }: { error: any; body: bodyType }) => {
     const chunks = Array.from({ length: 9 }, (_, i) => 0 + i)
+    const createdIds: Array<string> = []
     Promise.all(
       chunks.map((idx) => {
         const data = { ...body, split_idx: idx, PUBLIC_API_PASSWORD: password }
@@ -29,6 +31,11 @@ updateIndex.post(
         })
           .then((response) => {
             if (!response.ok) throw new Error(`Error while updating index ${body?.index_name} (chunk ${idx})`)
+            return response.json()
+          })
+          .then((data) => {
+            if (data.status === "success" && data?.data?.task_status) createdIds.push(data.data.task_status)
+            else throw new Error(`BSO returned status=${data.status}`)
           })
           .catch((error) => {
             throw error
@@ -42,6 +49,7 @@ updateIndex.post(
 
     return {
       chunks: chunks.length,
+      ids: createdIds,
       params: body,
     }
   },
