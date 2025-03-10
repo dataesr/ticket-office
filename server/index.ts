cd0971892c5b811fe79321931cafa93aac2544af
Mihoub <debache.mihoub@gmail.com>
2025-03-10 16:11:04 +0100

    fix(ci): test

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import { Elysia } from "elysia";
 import staticPlugin from "@elysiajs/static";
 import { swagger } from "@elysiajs/swagger";
-import dotenv from "dotenv";
 
 import contributionObjectRoutes from "./routes/contributionObjectRoutes";
 import productionsRoutes from "./routes/productions";
@@ -14,8 +13,6 @@ import getLastMailsSentRoutes from "./routes/last-mails-sent";
 import variationsRoutes from "./routes/variations";
 import storageRoutes from "./routes/storage";
 import bsoTasksRoutes from "./routes/bso-tasks";
-
-dotenv.config();
 
 const ENV = Bun.env.NODE_ENV || "development";
 
@@ -84,6 +81,7 @@ const createApp = async () => {
         })
         .use(staticPlugin({ prefix: "", alwaysStatic: true }))
         .get("*", () => Bun.file("./public/index.html"))
+        .compile()
         .listen(3000);
     default:
       return new Elysia()
@@ -146,7 +144,8 @@ const createApp = async () => {
           app.use(bsoTasksRoutes);
           return app;
         })
-        .listen(3000);
+        .listen(3000)
+        .compile();
   }
 };
 
