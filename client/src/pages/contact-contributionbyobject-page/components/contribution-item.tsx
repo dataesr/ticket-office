1e5017a139d9e92f2e97538335fd8fb60a44aa0c
Mihoub Debache <debache.mihoub@gmail.com>
2026-08-31 12:32:56 +0200

    fix(update contribution item header style

--- client/src/pages/contact-contributionbyobject-page/components/contribution-item.tsx
+++ client/src/pages/contact-contributionbyobject-page/components/contribution-item.tsx
@@ -37,47 +37,47 @@ const ContributionItem: React.FC<ContributionItemProps> = ({
     : "";
 
   return (
-    <div className="contribution-item-container">
-      <ul className="fr-badges-group fr-mb-3w">
-        {data?.tags?.length > 0 && (
-          <li>
-            <Badge color="green-menthe">{data.tags.join(", ")}</Badge>
-          </li>
-        )}
-        {data?.status && (
-          <li>
-            <Badge color={BadgeStatus({ status: data.status })}>
-              {StatusLabel({ status: data.status })}
-            </Badge>
-          </li>
-        )}
-        {firstResponse?.team && (
-          <li>
-            <Badge color="blue-ecume">{`Réponse : ${firstResponse.team}`}</Badge>
-          </li>
-        )}
-        {data?.comment && data?.team?.length > 0 && (
-          <li>
-            <Badge color="green-emeraude">{`Commenté par ${data.team[0]}`}</Badge>
-          </li>
-        )}
-        {data?.objectType && (
-          <li>
-            <Badge
-              color={BadgeColor({ type: data.objectType })}
-              icon={typeIcon({ icon: data.objectType })}
-            >
-              {TypeLabel({ type: data.objectType })}
-            </Badge>
-          </li>
-        )}
-      </ul>
+    <div className="contribution-item">
+      <div className="contribution-item__header">
+        <ul className="fr-badges-group fr-mb-1w">
+          {data?.tags?.length > 0 && (
+            <li>
+              <Badge color="green-menthe">{data.tags.join(", ")}</Badge>
+            </li>
+          )}
+          {data?.status && (
+            <li>
+              <Badge color={BadgeStatus({ status: data.status })}>
+                {StatusLabel({ status: data.status })}
+              </Badge>
+            </li>
+          )}
+          {firstResponse?.team && (
+            <li>
+              <Badge color="blue-ecume">{`Réponse : ${firstResponse.team}`}</Badge>
+            </li>
+          )}
+          {data?.comment && data?.team?.length > 0 && (
+            <li>
+              <Badge color="green-emeraude">{`Commenté par ${data.team[0]}`}</Badge>
+            </li>
+          )}
+          {data?.objectType && (
+            <li>
+              <Badge
+                color={BadgeColor({ type: data.objectType })}
+                icon={typeIcon({ icon: data.objectType })}
+              >
+                {TypeLabel({ type: data.objectType })}
+              </Badge>
+            </li>
+          )}
+        </ul>
 
-      <div className="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-        <div className="fr-col-12 fr-col-md-8">
-          <h2 className="fr-h5 fr-mb-1w">
+        <div className="contribution-item__header-row">
+          <h2 className="fr-h5 fr-mb-0">
             {data?.name || ""}
-            <span className="fr-ml-1w fr-text--regular">
+            <span className="fr-ml-1w fr-text--regular fr-text--sm">
               ({data?.id || ""})
               <CopyButton
                 text={data?.id || ""}
@@ -88,29 +88,29 @@ const ContributionItem: React.FC<ContributionItemProps> = ({
               />
             </span>
           </h2>
-
-          {!firstResponse && (
-            <div className="fr-alert fr-alert--info fr-alert--sm fr-mb-2w">
-              <p>Aucune réponse apportée à ce message pour l'instant</p>
-            </div>
-          )}
-        </div>
-        <div className="fr-col-12 fr-col-md-4 fr-text--right">
           {createdDate && (
-            <p className="fr-text--sm fr-text-mention--grey fr-mb-0">
-              <i>Reçu le {createdDate}</i>
+            <p className="fr-text--sm fr-text-mention--grey fr-mb-0 contribution-item__date">
+              Reçu le {createdDate}
             </p>
           )}
         </div>
       </div>
 
-      <MessagePreview
-        data={data}
-        allTags={allTags}
-        refetch={refetch}
-        highlightedQuery={highlightedQuery}
-      />
-      <StaffActions url={url} refetch={refetch} data={data} />
+      <div className="contribution-item__body">
+        {!firstResponse && (
+          <div className="fr-alert fr-alert--info fr-alert--sm fr-mb-3w">
+            <p>Aucune réponse apportée à ce message pour l'instant</p>
+          </div>
+        )}
+
+        <MessagePreview
+          data={data}
+          allTags={allTags}
+          refetch={refetch}
+          highlightedQuery={highlightedQuery}
+        />
+        <StaffActions url={url} refetch={refetch} data={data} />
+      </div>
     </div>
   );
 };
